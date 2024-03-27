5487fe25d2d8c81887ba24910549a3cfc53e5e23
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-03-27 23:00:55 +0530

    styles

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -11,7 +11,7 @@ export const BlogCards = ({
     content,
     publishedDate
 }: BlogCardProps) => {
-    return <>
+    return <div className="border border-slate-400 pb-4">
         <div className="flex">
             <div className="flex justify-center flex-col">
                 <Avatar name={authorName} />
@@ -24,25 +24,28 @@ export const BlogCards = ({
                 {publishedDate}
             </div>
         </div>
-        <div>
+        <div className="text-xl font-semibold">
             {title}
         </div>
-        <div>
+        <div className="text-md font-thin">
             {content.slice(0, 100) + "..."}
         </div>
-        <div>
+        <div className="w-full text-slate-400">
             {`${Math.ceil(content.length / 100)} minutes`}
         </div>
-        <div className="bg-slate-200 h-1 w-full">
+    </div>
+}
 
-        </div>
-    </>
+function Circle() {
+    return <div className="h-1 w-1 rounded-full bg-slate-200">
+
+    </div>
 }
 
 function Avatar({ name }: { name: string }) {
     return <div className="relative inline-flex items-center justify-center w-4 h-4 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-        <span className="text-sm text-gray-600 dark:text-gray-300">
+        <span className="text-sm text-red-600 dark:text-gray-300 pb-1">
             {name[0]}
         </span>
     </div>
-}
+}       
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -2,9 +2,9 @@ import { BlogCards } from "../components/BlogCard"
 
 export const Blogs = () => {
     return (
-        <div>
+        <div className="flex justify-center max-w-lg">
             <BlogCards authorName={"ash"}
-                title={"title of blog"}
+                title={"How an ungly frontend page makes 5000 dollars a month"}
                 content={"content of blog"}
                 publishedDate={"2nd feb 2024"}
             />
